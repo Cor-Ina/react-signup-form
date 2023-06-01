7a7430a3bc9db865d1c848f457af771b5a1de199
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2023-06-01 17:33:47 -0400

    Update css

--- src/App.js
+++ src/App.js
@@ -3,82 +3,95 @@ import React from "react"
 import './App.css';
 
 export default function App() {
+    const [formData, setFormData] = React.useState({
+        email: "",
+        password: "",
+        passwordConfirm: "",
+        joinedNewsletter: true
+    })
     
-  const [form, setForm] = React.useState( {
-    email: "",
-    password: "",
-    confirm: "",
-    joinedNewsletter: false,
-  })
-
+    /**
+     * Challenge: Connect the form to local state
+     * 
+     * 1. Create a state object to store the 4 values we need to save.
+     * 2. Create a single handleChange function that can
+     *    manage the state of all the inputs and set it up
+     *    correctly
+     * 3. When the user clicks "Sign up", check if the 
+     *    password & confirmation match each other. If
+     *    so, log "Successfully signed up" to the console.
+     *    If not, log "passwords do not match" to the console.
+     * 4. Also when submitting the form, if the person checked
+     *    the "newsletter" checkbox, log "Thanks for signing
+     *    up for our newsletter!" to the console.
+     */
     
-  function handleChange(event) {
-    const {name, type, value, checked} = event.target
-    setForm(prevForm => ({
-      
-      ...prevForm,
-      [name]: type === "checkbox" ? checked : value
-    }))
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault()
-    if(form.password === form.confirm) {
-        console.log("Successfully signed up")
-    } else {
-        console.log("Passwords do not match")
-        return
+    function handleChange(event) {
+        const {name, value, type, checked} = event.target
+        setFormData(prevFormData => ({
+            ...prevFormData,
+            [name]: type === "checkbox" ? checked : value
+        }))
     }
     
-    if(form.joinedNewsletter) {
-        console.log("Thanks for signing up for our newsletter!")
+    function handleSubmit(event) {
+        event.preventDefault()
+        if(formData.password === formData.passwordConfirm) {
+            console.log("Successfully signed up")
+        } else {
+            console.log("Passwords do not match")
+            return
+        }
+        
+        if(formData.joinedNewsletter) {
+            console.log("Thanks for signing up for our newsletter!")
+        }
     }
-  }
     
-  return (
-    <div className="form-container">
-        <form className="form" onSubmit={handleSubmit}>
-            <input 
-                type="email" 
-                placeholder="Email address"
-                className="form--input"
-                name="email"
-                value = {form.email}
-                onChange={handleChange}
-            />
-            <input 
-                type="password" 
-                placeholder="Password"
-                className="form--input"
-                name="password"
-                value = {form.password}
-                onChange={handleChange}
-            />
-            <input 
-                type="password" 
-                placeholder="Confirm password"
-                className="form--input"
-                name="confirm"
-                value = {form.confirm}
-                onChange={handleChange}
-            />
-            
-            <div className="form--marketing">
-                <input
-                    id="okayToEmail"
-                    type="checkbox"
-                    name="okayToEmail"
-                    checked = {form.joinedNewsletter}
+    return (
+        <div className="form-container">
+            <form className="form" onSubmit={handleSubmit}>
+                <input 
+                    type="email" 
+                    placeholder="Email address"
+                    className="form--input"
+                    name="email"
                     onChange={handleChange}
+                    value={formData.email}
                 />
-                <label htmlFor="okayToEmail">I want to join the newsletter</label>
-            </div>
-            <button 
-                className="form--submit"
-            >
-                Sign up
-            </button>
-        </form>
-    </div>
-  )
+                <input 
+                    type="password" 
+                    placeholder="Password"
+                    className="form--input"
+                    name="password"
+                    onChange={handleChange}
+                    value={formData.password}
+                />
+                <input 
+                    type="password" 
+                    placeholder="Confirm password"
+                    className="form--input"
+                    name="passwordConfirm"
+                    onChange={handleChange}
+                    value={formData.passwordConfirm}
+                />
+                
+                <div className="form--marketing">
+                    <input
+                        id="okayToEmail"
+                        type="checkbox"
+                        name="joinedNewsletter"
+                        onChange={handleChange}
+                        checked={formData.joinedNewsletter}
+                    />
+                    <label htmlFor="okayToEmail">I want to join the newsletter</label>
+                </div>
+                <button 
+                    className="form--submit"
+                >
+                    Sign up
+                </button>
+            </form>
+        </div>
+    )
 }
